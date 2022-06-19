2a97c2e34b38b3dc56ae572938cc78bc6d642f20
tomcohen2 <tomerc2010@gmail.com>
2022-06-19 12:01:44 +0300

    fonts-fix

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,9 @@ import './styles/HomePage.css'
 
 function HomePage(props) {
     return (
-        <div className='home__container'><h1>{props.title}</h1></div>
+        <div className='home__container'><h1>{props.title}</h1>
+            <h3>
+                The revolution is here, Stay Connected!</h3></div>
     )
 }
 
